8a7b8550eae066f8f278a6ac18b61e1e52b3a60b
bodhibrdr <julie.gumerman@gmail.com>
2019-08-19 16:14:23 -0600

    And we are fully functional. Not pretty, but functional.

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -2,10 +2,11 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Movie = (props) => {
-  const [movie, setMovie] = useState({});
+  const [movie, setMovie] = useState();
  
   useEffect(() => {
-    const id = 1;
+    const id = props.match.params.id;
+
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
@@ -42,12 +43,16 @@ const Movie = (props) => {
           Metascore: <strong>{metascore}</strong>
         </div>
         <h3>Actors</h3>
+        {!stars ? (
+  <h2>Loading stars..</h2>
+) : (
+  stars.map(star => (
+    <div key={star} className="movie-star">
+      {star}
+    </div>
+  ))
+)}
 
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
       </div>
       <div className="save-button">Save</div>
     </div>
